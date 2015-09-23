0ce7a0ead82872f5427ee36d210b10caea4f3b63
Rafael Schloming <rhs@alum.mit.edu>
2015-09-23 15:36:10 -0400

    reordered, renamed, and split out renderers in preparation for multiple backends

--- quark/backend.py
+++ quark/backend.py
@@ -27,7 +27,7 @@ class Java(Backend):
 
     def __init__(self):
         Backend.__init__(self)
-        self.classr = ClassRenderer()
+        self.dfnr = DefinitionRenderer()
         self.functions = []
 
     def write(self, target):
@@ -53,11 +53,11 @@ class Java(Backend):
         """)
 
     def visit_Class(self, c):
-        pkg = self.classr.namer.package(c)
+        pkg = self.dfnr.namer.package(c)
         if pkg:
-            self.files["%s/%s.java" % (pkg, c.name.text)] = c.match(self.classr)
+            self.files["%s/%s.java" % (pkg, c.name.text)] = c.match(self.dfnr)
         else:
-            self.files["%s.java" % c.name.text] = c.match(self.classr)
+            self.files["%s.java" % c.name.text] = c.match(self.dfnr)
 
     def visit_Function(self, f):
         if not isinstance(f, Method):
@@ -67,13 +67,13 @@ class Java(Backend):
         if self.functions:
             packages = OrderedDict()
             for f in self.functions:
-                pkg = self.classr.namer.package(f)
+                pkg = self.dfnr.namer.package(f)
                 if pkg in packages:
                     functions = packages[pkg]
                 else:
                     functions = []
                     packages[pkg] = functions
-                functions.append(f.match(self.classr))
+                functions.append(f.match(self.dfnr))
                 if f.name.text == "main":
                     functions.append("public static void main(String[] args) {\n    main();\n}")
             for pkg, functions in packages.items():
@@ -93,44 +93,149 @@ def indent(st, level=4):
     else:
         return ""
 
-class NameRenderer(object):
+class DocEvaluator:
 
-    def match_Name(self, n):
-        return n.text
+    def __init__(self):
+        self.lines = []
 
-    def match_Type(self, t):
-        if t.parameters:
-            params = [p.match(self) for p in t.parameters]
-            return "%s<%s>" % (".".join([p.match(self) for p in t.path]), ",".join(params))
+    @property
+    def doc(self):
+        if self.lines:
+            lines = ["/**"]
+            lines.extend([(" * %s" % l).rstrip() for l in self.lines])
+            lines.append(" */")
+            lines.append("")
+            return "\n".join(lines)
         else:
-            return ".".join([p.match(self) for p in t.path])
+            return ""
+
+    def match_Annotation(self, a):
+        for e in a.arguments:
+            e.match(self)
+
+    def match_String(self, s):
+        # XXX: need to properly process string literal
+        self.lines.append(s.text[1:-1])
+
+class DefinitionRenderer(object):
+
+    def __init__(self):
+        self.namer = SubstitutionNamer({"self": "this", "int": "Integer",
+                                        "List": "java.util.ArrayList",
+                                        "Map": "java.util.HashMap"})
+        self.stmtr = StatementRenderer(self.namer)
+
+    def doc(self, annotations):
+        doc_eval = DocEvaluator()
+        for a in annotations:
+            if a.name.text == "doc":
+                a.match(doc_eval)
+        return doc_eval.doc
+
+    def match_Class(self, c):
+        name = c.name.match(self.namer)
+        params = ""
+        if c.parameters:
+            params = "<%s>" % (", ".join([p.match(self) for p in c.parameters]))
+        body = "\n".join([d.match(self) for d in c.definitions])
+        kw = "interface" if isinstance(c, Interface) else "class"
+        doc = self.doc(c.annotations)
+        cls = "%spublic %s %s%s {%s}" % (doc, kw, name, params, indent(body))
+        pkg = self.namer.package(c)
+        if pkg:
+            return "package %s;\n\n%s" % (pkg, cls)
+        else:
+            return cls
+
+    def match_TypeParam(self, p):
+        return p.name.match(self.namer)
+
+    def match_Function(self, m):
+        doc = self.doc(m.annotations)
+        type = "%s " % m.type.match(self.namer) if m.type else ""
+        name = m.name.match(self.namer)
+        params = ", ".join([p.match(self) for p in m.params])
+        body = " {%s}" % indent(m.body.match(self.stmtr)) if m.body else ";"
+        if isinstance(m, Method):
+            mods = "public"
+        else:
+            mods = "public static"
+        return "%s%s %s%s(%s)%s" % (doc, mods, type, name, params, body)
+
+    def match_MethodMacro(self, mm):
+        return ""
+
+    def match_Field(self, f):
+        doc = self.doc(f.annotations)
+        return "%s%s;" % (doc, f.match(self.stmtr))
+
+    def match_Param(self, p):
+        return p.match(self.stmtr)
+
+class StatementRenderer(object):
+
+    def __init__(self, namer):
+        self.namer = namer
+        self.exprr = ExprRenderer(self.namer)
+
+    @dispatch(TypeExpr)
+    def type(self, texpr):
+        return self.type(texpr.type, texpr.bindings)
+
+    @dispatch(Class)
+    def type(self, cls, bindings):
+        if cls.parameters:
+            params = [self.type(bindings[p]) for p in cls.parameters]
+            return "%s<%s>" % (cls.name.match(self.namer), ",".join(params))
+        else:
+            return cls.name.match(self.namer)
+
+    def maybe_cast(self, type, expr):
+        result = expr.match(self.exprr)
+        if type.resolved.id != expr.resolved.id:
+            result = "(%s) (%s)" % (self.type(type.resolved), result)
+        return result
+
+    def match_Return(self, r):
+        return "return %s;" % self.maybe_cast(r.callable.type, r.expr)
+
+    def match_Local(self, stmt):
+        return "%s;" % stmt.declaration.match(self)
+
+    def match_Declaration(self, d):
+        type = d.type.match(self.namer)
+        name = d.name.match(self.namer)
+        if d.value:
+            value = self.maybe_cast(d.type, d.value)
+            return "%s %s = %s" % (type, name, value)
+        else:
+            return "%s %s" % (type, name)
+
+    def match_ExprStmt(self, stmt):
+        return "%s;" % stmt.expr.match(self.exprr)
+
+    def match_Assign(self, a):
+        return "%s = %s;" % (a.lhs.match(self), self.maybe_cast(a.lhs, a.rhs))
+
+    def match_Attr(self, a):
+        return "(%s).%s" % (a.expr.match(self.exprr), a.attr.text)
 
     def match_Var(self, v):
-        return v.name.match(self)
+        return v.match(self.namer)
 
-    def package(self, node):
-        if isinstance(node, Package):
-            me = node.name.match(self)
-            parent = self.package(node.parent)
-            if parent:
-                return "%s.%s" % (parent, me)
-            else:
-                return me
-        elif node.parent:
-            return self.package(node.parent)
-        else:
-            return None
+    def match_If(self, i):
+        result = "if (%s) {%s}" % (i.predicate.match(self.exprr),
+                                   indent(i.consequence.match(self)))
+        if i.alternative:
+            result += " else {%s}" % indent(i.alternative.match(self))
+        return result
 
-class SubstitutionNamer(NameRenderer):
+    def match_While(self, w):
+        return "while (%s) {%s}" % (w.condition.match(self.exprr),
+                                    indent(w.body.match(self)))
 
-    def __init__(self, env):
-        self.env = env
-
-    def match_Name(self, n):
-        if n.text in self.env:
-            return self.env[n.text]
-        else:
-            return n.text
+    def match_Block(self, b):
+        return "\n".join([s.match(self) for s in b.statements])
 
 class ExprRenderer(object):
 
@@ -231,137 +336,42 @@ class ExprRenderer(object):
             idx += 1
         return method_macro.body.match(ExprRenderer(SubstitutionNamer(env)))
 
-class DocEvaluator:
+class NameRenderer(object):
 
-    def __init__(self):
-        self.lines = []
+    def match_Name(self, n):
+        return n.text
 
-    @property
-    def doc(self):
-        if self.lines:
-            lines = ["/**"]
-            lines.extend([(" * %s" % l).rstrip() for l in self.lines])
-            lines.append(" */")
-            lines.append("")
-            return "\n".join(lines)
+    def match_Type(self, t):
+        if t.parameters:
+            params = [p.match(self) for p in t.parameters]
+            return "%s<%s>" % (".".join([p.match(self) for p in t.path]), ",".join(params))
         else:
-            return ""
-
-    def match_Annotation(self, a):
-        for e in a.arguments:
-            e.match(self)
-
-    def match_String(self, s):
-        # XXX: need to properly process string literal
-        self.lines.append(s.text[1:-1])
-
-class ClassRenderer(object):
-
-    def __init__(self):
-        self.namer = SubstitutionNamer({"self": "this", "int": "Integer",
-                                        "List": "java.util.ArrayList",
-                                        "Map": "java.util.HashMap"})
-        self.exprr = ExprRenderer(self.namer)
-
-    def doc(self, annotations):
-        doc_eval = DocEvaluator()
-        for a in annotations:
-            if a.name.text == "doc":
-                a.match(doc_eval)
-        return doc_eval.doc
-
-    def match_Class(self, c):
-        name = c.name.match(self.namer)
-        params = ""
-        if c.parameters:
-            params = "<%s>" % (", ".join([p.match(self) for p in c.parameters]))
-        body = "\n".join([d.match(self) for d in c.definitions])
-        kw = "interface" if isinstance(c, Interface) else "class"
-        doc = self.doc(c.annotations)
-        cls = "%spublic %s %s%s {%s}" % (doc, kw, name, params, indent(body))
-        pkg = self.namer.package(c)
-        if pkg:
-            return "package %s;\n\n%s" % (pkg, cls)
-        else:
-            return cls
-
-    def match_TypeParam(self, p):
-        return p.name.match(self.namer)
-
-    def match_Block(self, b):
-        return "\n".join([s.match(self) for s in b.statements])
-
-    def match_Function(self, m):
-        doc = self.doc(m.annotations)
-        type = "%s " % m.type.match(self.namer) if m.type else ""
-        name = m.name.match(self.namer)
-        params = ", ".join([p.match(self) for p in m.params])
-        body = " {%s}" % indent(m.body.match(self)) if m.body else ";"
-        if isinstance(m, Method):
-            mods = "public"
-        else:
-            mods = "public static"
-        return "%s%s %s%s(%s)%s" % (doc, mods, type, name, params, body)
-
-    def match_MethodMacro(self, mm):
-        return ""
-
-    @dispatch(TypeExpr)
-    def type(self, texpr):
-        return self.type(texpr.type, texpr.bindings)
-
-    @dispatch(Class)
-    def type(self, cls, bindings):
-        if cls.parameters:
-            params = [self.type(bindings[p]) for p in cls.parameters]
-            return "%s<%s>" % (cls.name.match(self.namer), ",".join(params))
-        else:
-            return cls.name.match(self.namer)
-
-    def maybe_cast(self, type, expr):
-        result = expr.match(self.exprr)
-        if type.resolved.id != expr.resolved.id:
-            result = "(%s) (%s)" % (self.type(type.resolved), result)
-        return result
-
-    def match_Declaration(self, d):
-        type = d.type.match(self.namer)
-        name = d.name.match(self.namer)
-        if d.value:
-            value = self.maybe_cast(d.type, d.value)
-            return "%s %s = %s" % (type, name, value)
-        else:
-            return "%s %s" % (type, name)
-
-    def match_Field(self, f):
-        doc = self.doc(f.annotations)
-        return "%s%s;" % (doc, self.match_Declaration(f))
-
-    def match_Return(self, r):
-        return "return %s;" % self.maybe_cast(r.callable.type, r.expr)
-
-    def match_Local(self, stmt):
-        return "%s;" % stmt.declaration.match(self)
-
-    def match_ExprStmt(self, stmt):
-        return "%s;" % stmt.expr.match(self.exprr)
-
-    def match_Assign(self, a):
-        return "%s = %s;" % (a.lhs.match(self), self.maybe_cast(a.lhs, a.rhs))
-
-    def match_Attr(self, a):
-        return "(%s).%s" % (a.expr.match(self.exprr), a.attr.text)
+            return ".".join([p.match(self) for p in t.path])
 
     def match_Var(self, v):
-        return v.match(self.namer)
+        return v.name.match(self)
 
-    def match_If(self, i):
-        result = "if (%s) {%s}" % (i.predicate.match(self.exprr),
-                                   indent(i.consequence.match(self)))
-        if i.alternative:
-            result += " else {%s}" % indent(i.alternative.match(self))
-        return result
+    def package(self, node):
+        if isinstance(node, Package):
+            me = node.name.match(self)
+            parent = self.package(node.parent)
+            if parent:
+                return "%s.%s" % (parent, me)
+            else:
+                return me
+        elif node.parent:
+            return self.package(node.parent)
+        else:
+            return None
 
-    def match_While(self, w):
-        return "while (%s) {%s}" % (w.condition.match(self.exprr),
-                                    indent(w.body.match(self)))
+class SubstitutionNamer(NameRenderer):
+
+    def __init__(self, env):
+        self.env = env
+
+    def match_Name(self, n):
+        if n.text in self.env:
+            return self.env[n.text]
+        else:
+            return n.text
+
