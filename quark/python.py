6dfaf7e0eea576d836df95a70a8e695ba94348da
Abhay Saxena <ark3@datawire.io>
2015-10-13 13:58:14 -0400

    Output Quark runtime as a library (Python)

--- quark/python.py
+++ quark/python.py
@@ -200,20 +200,8 @@ class Python(backend.Backend):
     def __init__(self):
         backend.Backend.__init__(self, "py")
         self.dfnr = PythonDefinitionRenderer()
-        self.header = """import os, sys
-
-_Map = dict
-class _List(list):
-    def __repr__(self):
-        return "[%s]" % ", ".join([str(e) for e in self])
-
-def _println(obj):
-    if obj is None:
-        sys.stdout.write("null\\n")
-    else:
-        sys.stdout.write("%s\\n" % obj)
-
-"""
+        self.header = "from quark_runtime import *\n\n"
+        self.files["quark_runtime.py"] = open(os.path.join(os.path.dirname(__file__), "quark_runtime.py")).read()
 
     def write(self, target):
         src = target
--- quark/test/emit/Macro/py/Macro.py
+++ quark/test/emit/Macro/py/Macro.py
@@ -1,15 +1,4 @@
-import os, sys
-
-_Map = dict
-class _List(list):
-    def __repr__(self):
-        return "[%s]" % ", ".join([str(e) for e in self])
-
-def _println(obj):
-    if obj is None:
-        sys.stdout.write("null\n")
-    else:
-        sys.stdout.write("%s\n" % obj)
+from quark_runtime import *
 
 class Macro(object):
     def _init(self): pass
