048bc941d9d9f71fa2eb9933c8475b03dccdf159
Rafael Schloming <rhs@alum.mit.edu>
2015-10-30 11:59:22 -0400

    made primitives extensible as interfaces

---
+++ quark/test/emit/extending-primitives/java/Functions.java
@@ -0,0 +1,11 @@
+public class Functions {
+    public static void main() {
+        pkg.C c = new pkg.C();
+        (c).event1();
+        (c).event2();
+        (c).run();
+    }
+    public static void main(String[] args) {
+        main();
+    }
+}
