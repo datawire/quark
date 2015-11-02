29ea49fab2506f14d936a8e4a15649263f0e4866
Bozo Dragojevic <bozzo@digiverse.si>
2015-11-02 14:34:14 +0100

    Specify java 7 in pom.xml

--- quark/java.py
+++ quark/java.py
@@ -23,11 +23,20 @@ pom_xml = """<?xml version="1.0" encoding="UTF-8"?>
 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
   <modelVersion>4.0.0</modelVersion>
   <groupId>org.apache.qpid</groupId>
-  <artifactId>test</artifactId>
+  <artifactId>test%(name)s</artifactId>
   <version>%(version)s</version>
   <name>%(name)s</name>
   <build>
     <plugins>
+      <plugin>
+        <groupId>org.apache.maven.plugins</groupId>
+        <artifactId>maven-compiler-plugin</artifactId>
+        <version>3.3</version>
+        <configuration>
+          <source>1.7</source>
+          <target>1.7</target>
+        </configuration>
+      </plugin>
       <plugin>
         <groupId>org.apache.maven.plugins</groupId>
         <artifactId>maven-javadoc-plugin</artifactId>
