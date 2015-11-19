0006f0e24c1f73ee2a6eea8ea34499088bdef0a7
Abhay Saxena <ark3@datawire.io>
2015-11-19 15:53:33 -0500

    Add required license/readme stuff

--- runtime/js-node/quark_node_runtime.js
+++ runtime/js-node/quark_node_runtime.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 datawire. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Quark's Node Runtime and associated
 /* jshint node: true */
 
@@ -8,7 +24,7 @@
 
     var WebSocket = require("ws");
 
-    var runtime = require("quark_runtime.js")
+    var runtime = require("quark_runtime.js");
 
     // CLASS QuarkWebsocket
     function QuarkWebSocket(url, handler) {
@@ -55,7 +71,7 @@
             return 1;
         }
         return 0;
-    }
+    };
 
     var http = require("http");
     var https = require("https");
@@ -146,7 +162,7 @@
     };
     Runtime.prototype.launch = function () {};
 
-    Runtime.prototype.codec = function() { return runtime.defaultCodec(); }
+    Runtime.prototype.codec = function() { return runtime.defaultCodec(); };
 
 
     module.exports = new Runtime();
