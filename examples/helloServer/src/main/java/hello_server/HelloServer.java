1cfb0f73d73b974aeac49b0ea3398cbd524ed5c7
Bozo Dragojevic <bozzo@digiverse.si>
2015-11-24 00:28:45 +0100

    implement HelloServer

--- examples/helloServer/src/main/java/hello_server/HelloServer.java
+++ examples/helloServer/src/main/java/hello_server/HelloServer.java
@@ -1,5 +1,12 @@
 package hello_server;
 
+import io.datawire.quark.netty.QuarkNettyRuntime;
+
 public class HelloServer {
-  
+    public static void main(String[] args) throws Exception {
+        QuarkNettyRuntime runtime = new QuarkNettyRuntime();
+        HelloServlet server = new HelloServlet();
+        runtime.serveHTTP("http://localhost:8765/foo", server);
+        runtime.launch();
+    }
 }
