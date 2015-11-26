982717e5807c8e8bc7fca20682802adbcf86e4e5
Abhay Saxena <ark3@datawire.io>
2015-11-26 10:27:57 -0600

    Add Py threaded runtime, just schedule support

---
+++ runtime/python-threaded/quark_threaded_runtime.py
@@ -0,0 +1,82 @@
+# Quark's Twisted Runtime and associated
+
+__version__ = "0.1.0"
+
+import threading
+import time
+from Queue import Queue
+
+
+class EventProcessor(threading.Thread):
+
+    QUIT = ("QUIT",)
+
+    def __init__(self, runtime):
+        super(EventProcessor, self).__init__()
+        self.runtime = runtime
+
+    def run(self):
+        while True:
+            event = self.runtime.events.get()
+            if event == self.QUIT:
+                return
+            function, args, kwargs = event
+            function(*args, **kwargs)
+
+
+class ThreadedRuntime(object):
+
+    def __init__(self):
+        self.lock = threading.Condition()
+        self.events = Queue()
+        self.event_thread = None
+
+    def acquire(self):
+        self.lock.acquire()
+
+    def release(self):
+        self.lock.release()
+
+    def _notify(self):
+        self.lock.acquire()
+        self.lock.notify_all()
+        self.lock.release()
+
+    def wait(self):
+        raise NotImplementedError()
+
+    def open(self, url, handler):
+        raise NotImplementedError()
+
+    def request(self, request, handler):
+        raise NotImplementedError()
+
+    def schedule(self, handler, delayInSeconds):
+        def run_scheduled(runtime, handler, delayInSeconds):
+            time.sleep(delayInSeconds)
+            runtime.events.put((handler.onExecute, [runtime], {}))
+        thread = threading.Thread(target=run_scheduled, args=(self, handler, delayInSeconds))
+        thread.setDaemon(True)
+        thread.start()
+
+    def serveHTTP(self, url, servlet):
+        raise NotImplementedError()
+
+    def respond(self, request, response):
+        raise NotImplementedError()
+
+    def launch(self):
+        assert self.event_thread is None, self.event_thread
+        self.event_thread = EventProcessor(self)
+        self.event_thread.start()
+
+    def finish(self):
+        self.events.put(EventProcessor.QUIT)
+        self.event_thread.join()
+
+
+_threaded_runtime = ThreadedRuntime()
+
+
+def get_threaded_runtime():
+    return _threaded_runtime
