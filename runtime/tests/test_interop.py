cefdec324e27248ea345b8b87fe25f9a972d5bf6
Bozo Dragojevic <bozzo@digiverse.si>
2015-12-09 17:14:33 +0100

    Add netty integration to the test, improve port allocation, catch bugs!

--- runtime/tests/test_interop.py
+++ runtime/tests/test_interop.py
@@ -78,6 +78,10 @@ class BackgroundProcess(object):
     def __init__(self, cwd, cmd):
         self.cwd = cwd
         self.cmd = cmd
+        self.stdout = None
+        self.stderr = None
+        self.p = None
+        self.w = None
 
     def start(self):
         print "background process", self.cwd, self.cmd
@@ -86,10 +90,11 @@ class BackgroundProcess(object):
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
         def comm():
-            self.stdout, self.stderr = self.p.communicate()
+            o, e = self.p.communicate()
+            print self.cmd, "produced", o, "and", e
+            self.stdout, self.stderr = o, e
 
         self.w = threading.Thread(target=comm)
-        self.w.daemon = True
         self.w.start()
 
     def stop(self, timeout=0.1):
@@ -141,17 +146,15 @@ class Integration(object):
     def rundir(self):
         return self.tmpdir / "run" / self.name
 
-    @property
-    def client(self):
+    def client(self, port):
         self.check_build()
         return BackgroundProcess(cwd=self.rundir,
-                                 cmd=self.invoke_client)
+                                 cmd=self.invoke_client(port))
 
-    @property
-    def server(self):
+    def server(self, port):
         self.check_build()
         return BackgroundProcess(cwd=self.rundir,
-                                 cmd=self.invoke_server)
+                                 cmd=self.invoke_server(port))
 
     @property
     def runtimes(self):
@@ -160,12 +163,10 @@ class Integration(object):
     def build(self):
         pytest.fail("Need to implement build")
 
-    @property
-    def invoke_server(self):
+    def invoke_server(self, port):
         pytest.fail("Need to implement invoke_server")
 
-    @property
-    def invoke_client(self):
+    def invoke_client(self, port):
         pytest.fail("Need to implement invoke_client")
 
 class Node(Integration):
@@ -181,14 +182,18 @@ class Node(Integration):
             var runtime = require("datawire-quark-node");
             var runtime_core = require("datawire-quark-core");
             var interop = require("interop");
-            new interop.Entrypoint().server(runtime, 9876);
+            var process = require("process");
+            console.log("Node client harness is started")
+            new interop.Entrypoint().client(runtime, process.argv[2]);
         """))
         self.rundir.join("run-server.js").write(textwrap.dedent("""\
             "use strict";
             var runtime = require("datawire-quark-node");
             var runtime_core = require("datawire-quark-core");
             var interop = require("interop");
-            new interop.Entrypoint().client(runtime, 9876);
+            var process = require("process");
+            console.log("Node server harness is started")
+            new interop.Entrypoint().server(runtime, process.argv[2]);
         """))
 
     def npm_install(self, package):
@@ -202,13 +207,11 @@ class Node(Integration):
     def js_node_package(self):
         return self.runtimes / "js-node"
 
-    @property
-    def invoke_server(self):
-        return ["node", "run-server.js"]
+    def invoke_server(self, port):
+        return ["node", "run-server.js", str(port)]
 
-    @property
-    def invoke_client(self):
-        return ["node", "run-client.js"]
+    def invoke_client(self, port):
+        return ["node", "run-client.js", str(port)]
 
 class Twisted(Integration):
     def __init__(self, **kwargs):
@@ -223,16 +226,20 @@ class Twisted(Integration):
             import interop
             import quark_twisted_runtime
             import quark_runtime
+            import sys
+            print "Twisted client harness is started"
             runtime = quark_twisted_runtime.get_twisted_runtime()
-            interop.Entrypoint().client(runtime, 9876)
+            interop.Entrypoint().client(runtime, int(sys.argv[1]))
             runtime.launch()
         """))
         self.rundir.join("run-server.py").write(textwrap.dedent("""\
             import interop
             import quark_twisted_runtime
             import quark_runtime
+            import sys
+            print "Twisted server harness is started"
             runtime = quark_twisted_runtime.get_twisted_runtime()
-            interop.Entrypoint().server(runtime, 9876)
+            interop.Entrypoint().server(runtime, int(sys.argv[1]))
             runtime.launch()
         """))
 
@@ -256,20 +263,126 @@ class Twisted(Integration):
     def py_twisted_package(self):
         return self.runtimes / "twisted"
 
-    @property
-    def invoke_server(self):
-        return [self.python.strpath, "run-server.py"]
+    def invoke_server(self, port):
+        return [self.python.strpath, "run-server.py", str(port)]
 
-    @property
-    def invoke_client(self):
-        return [self.python.strpath, "run-client.py"]
+    def invoke_client(self, port):
+        return [self.python.strpath, "run-client.py", str(port)]
 
 class Netty(Integration):
     def __init__(self, **kwargs):
         super(Netty, self).__init__(**kwargs)
 
     def build(self):
-        print "TODO, build java"
+        self.m2_repo.ensure(dir=1)
+        self.rundir.join("pom.xml").write(textwrap.dedent("""\
+            <?xml version="1.0" encoding="UTF-8"?>
+            <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+              <modelVersion>4.0.0</modelVersion>
+              <groupId>io.datawire.quark.interop-test</groupId>
+              <artifactId>interop-harness</artifactId>
+              <version>0.0.1-SNAPSHOT</version>
+              <name>interop harness</name>
+              <build>
+                <plugins>
+                  <plugin>
+                    <groupId>org.apache.maven.plugins</groupId>
+                    <artifactId>maven-compiler-plugin</artifactId>
+                    <version>3.3</version>
+                    <configuration>
+                      <source>1.7</source>
+                      <target>1.7</target>
+                    </configuration>
+                  </plugin>
+                </plugins>
+              </build>
+              <repositories>
+               <repository>
+                <id>test repo</id>
+                <url>file://%(m2_repo)s</url>
+               </repository>
+              </repositories>
+              <dependencies>
+                <dependency>
+                  <groupId>io.datawire.quark</groupId>
+                  <artifactId>quark-netty</artifactId>
+                  <version>%(quark_netty_version)s</version>
+                  <scope>compile</scope>
+                </dependency>
+                <dependency>
+                  <groupId>io.datawire.quark.interop-test</groupId>
+                  <artifactId>interop-quark</artifactId>
+                  <version>%(compile_version)s</version>
+                </dependency>
+              </dependencies>
+            </project>
+        """ % dict(quark_netty_version = self.quark_netty_version,
+                   m2_repo = self.m2_repo.strpath,
+                   compile_version = self.compile.version,
+                   ))
+        )
+        harness = self.rundir / "src" / "main" / "java" / "interop"/ "harness"
+        harness.ensure(dir=1)
+        harness.join("Client.java").write(textwrap.dedent("""\
+            package interop.harness;
+            import io.datawire.quark.netty.QuarkNettyRuntime;
+            public class Client {
+                public static void main(String[] args) {
+                    System.out.println("Netty client harness is started");
+                    QuarkNettyRuntime runtime = new QuarkNettyRuntime();
+                    new interop.Entrypoint().client(runtime, Integer.parseInt(args[0]));
+                }
+            }
+        """)
+        )
+        harness.join("Server.java").write(textwrap.dedent("""\
+            package interop.harness;
+            import io.datawire.quark.netty.QuarkNettyRuntime;
+            public class Server {
+                public static void main(String[] args) {
+                    System.out.println("Netty server harness is started");
+                    QuarkNettyRuntime runtime = new QuarkNettyRuntime();
+                    new interop.Entrypoint().server(runtime, Integer.parseInt(args[0]));
+                }
+            }
+        """)
+        )
+        command("mvn", "install::install-file",
+                "-Dfile=%s" % self.compile.java_package.strpath,
+                "-DgroupId=io.datawire.quark.interop-test",
+                "-DartifactId=interop-quark",
+                "-Dversion=%s" % self.compile.version,
+                "-Dpackaging=jar",
+                "-DlocalRepositoryPath=%s" % self.m2_repo.strpath,
+                cwd=self.rundir)
+        command("mvn", "compile", cwd=self.rundir)
+
+    @property
+    def quark_netty_version(self):
+        pom = self.quark_netty_package.join("pom.xml").read()
+        version = pom.split("<version>",1)[1].split("</version>",1)[0]
+        return version
+
+    @property
+    def quark_netty_package(self):
+        return self.runtimes / "netty"
+
+    @property
+    def m2_repo(self):
+        return self.rundir / "m2repo"
+
+    def invoke_client(self, port):
+        return ["mvn", "exec:java",
+                "-Dexec.mainClass=interop.harness.Client",
+                "-Dexec.args=%s" % port,
+                "-q"]
+
+    def invoke_server(self, port):
+        return ["mvn", "exec:java",
+                "-Dexec.mainClass=interop.harness.Server",
+                "-Dexec.args=%s" % port,
+                "-q"]
+
 
 directory = os.path.join(os.path.dirname(__file__), "interop")
 
@@ -295,6 +408,22 @@ class IntegrationCache(object):
             self.integrations[key] = cls(compile=compile)
         return self.integrations[key]
 
+class PortGenerator(object):
+    def __init__(self):
+        self.port = 9876
+
+    def next(self):
+        self.port += 1
+        return self.port
+
+@pytest.fixture(scope='module')
+def port_generator():
+    return PortGenerator()
+
+@pytest.fixture
+def port(port_generator):
+    return port_generator.next()
+
 @pytest.fixture(params=paths)
 def path(request):
     return request.param
@@ -319,11 +448,22 @@ def client_integration(request, compile, integration_cache):
 def server_integration(request, compile, integration_cache):
     return integration_cache.get(request.param, compile)
 
-def test_interop(client_integration, server_integration):
-    client = client_integration.client
-    server = server_integration.server
-    with server.wait(0), client.wait(1):
-        pass
-    assert "FAIL" not in client.stdout
+def test_interop(client_integration, server_integration, port):
+    client = client_integration.client(port)
+    server = server_integration.server(port)
+    with server.wait(0):
+        time.sleep(1)
+        with client.wait(1):
+            print "server and client started"
+            time.sleep(1)
+    print "server and client stopped"
+    c = client.stdout
+    assert "FAIL" not in c
+    assert "onHTTPInit" in c
+    assert "onHTTPFinal" in c
+    assert "onHTTPError" not in c
+    s = server.stdout
+    assert "Hello Registered on" in s
+    assert "Hello Trouble" not in s
     assert client.stderr == ""
     assert server.stderr == ""
