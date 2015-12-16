90175b458dc66c9069fc2db3a28fd82fed8c6216
Abhay Saxena <ark3@datawire.io>
2015-12-16 13:07:44 -0500

    Update slack example bot.py for the py-threaded integration

--- examples/slack-bot/bot.py
+++ examples/slack-bot/bot.py
@@ -1,7 +1,6 @@
 # Connect to Slack
 
-from quark_twisted_runtime import get_twisted_runtime as bot_get_runtime
-#from quark_threaded_runtime import get_threaded_runtime as bot_get_runtime
+from quark_threaded_runtime import get_runtime
 import slack
 
 
@@ -24,6 +23,7 @@ class Handler(slack.SlackHandler):
     def onMessageSent(self, message):
         print "Acknowledged:", message.text
 
+
 def main():
     try:
         token = open(".slack.token").read().strip()
@@ -31,11 +31,11 @@ def main():
         # you need to go to https://api.slack.com/web and generate an access token
         exit("Failed to read Slack token. See examples/README.md for more information.")
 
-    runtime = bot_get_runtime()
+    runtime = get_runtime()
     client = slack.Client(runtime, token)
     client.post("#demo", "testing...")
     client.subscribe(Handler())
-    runtime.launch()
+    runtime.join()
 
 
 if __name__ == '__main__':
