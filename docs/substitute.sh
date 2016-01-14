195676bc16bb949a9b81a079f923dffbe50bcc73
brikelly <bkelly@datawire.io>
2016-01-14 14:56:33 -0500

    Adding substitution script for replacing product name strings in the generated HTML documentation.

---
+++ docs/substitute.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# set -o verbose
+
+# This script uses the directives in variables.txt to process all the HTML files
+# within the docs directory, replacing any strings of the form {{{term}}} with
+# their actual values. Strings are replaced in-place.
+# The file "replace-test.dat" is used as the basis for a test HTML file, and
+# it contains samples of all the current substitution codes.
+
+cp replace-test.dat replace-test-copy.html
+while read p; do
+  tofind=$(echo $p | awk -F';' '{print $1}')
+  torepl=$(echo $p | awk -F';' '{print $2}')
+  cmd="find . -type f -name '*.html' -print0 | xargs -0 perl -pi -e 's#$tofind#$torepl#g' "
+  echo Replacing $tofind with $torepl
+  eval $cmd
+done<variables.txt
+echo Done.
