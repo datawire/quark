3487060e7523bd253c3c6e5e6753106d1ecf9151
Franklin <ffffranklin@pobox.com>
2016-01-18 11:06:42 -0800

    #98 Implementing initial layout sidebar and updating gitignore

--- docs/conf.py
+++ docs/conf.py
@@ -162,7 +162,14 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {
+  '**': [
+    'globaltoc.html',
+    'localtoc.html',
+    'relations.html',
+    'customsidebar.html',
+  ]
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
