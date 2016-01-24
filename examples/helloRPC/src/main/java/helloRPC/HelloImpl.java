2745f30a9f091e9438536eb69250248dc0a1b6ee
brikelly <bkelly@datawire.io>
2016-01-24 15:56:15 -0500

    Adding timeout/local server changes to Java helloRPC example

--- examples/helloRPC/src/main/java/helloRPC/HelloImpl.java
+++ examples/helloRPC/src/main/java/helloRPC/HelloImpl.java
@@ -9,6 +9,15 @@ public class HelloImpl extends ServerServiceStub implements Hello {
     @Override
     public Response hello(Request request) {
         Response response = new Response();
+
+        // Uncomment this try block to simulate a long request processing
+        // time and force a request timeout to occur for the client.
+        // try {
+        //   Thread.sleep(5000);
+        // }
+        // catch(InterruptedException ex) {
+        // }
+
         response.result = "Responding to [" + request.text + "] from Java";
         return response;
     }
--- examples/helloRPC/src/main/java/helloRPC/HelloRPCClient.java
+++ examples/helloRPC/src/main/java/helloRPC/HelloRPCClient.java
@@ -9,9 +9,26 @@ public class HelloRPCClient {
     public static void main(String[] args) {
         QuarkNettyRuntime runtime = QuarkNettyRuntime.getRuntime();
         runtime.setAllowSync(true);
-        HelloClient client = new HelloClient(runtime, "http://localhost:8910/hello");
+
+        // "http://hello.datawire.io/" is the URL of the simple "Hello" cloud
+        // microservice run by Datawire, Inc. to serve as a simple first test.
+        //
+        //  You can test completely locally, too:
+        //  - comment out the http://hello.datawire.io line
+        //  - uncomment the http://127.0.0.1:8910/hello line
+        //  - fire up the local version of the server by following the instructions
+        //  in the README.md.
+        //
+        HelloClient client = new HelloClient(runtime, "http://hello.datawire.io/");
+        // HelloClient client = new HelloClient(runtime, "http://localhost:8910/hello");
+
         Request request = new Request();
-        request.text = "Hello from Java!";
+
+        if (args.length > 0) {
+            request.text = args[0];
+        } else {
+            request.text = "Hello from Java!";
+        }
         System.out.println("Request says: " + request.text);
         Response response = client.hello(request);
         if (response == null) {
