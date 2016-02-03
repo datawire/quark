9a1744754feb8f84c7fdd18cebf0d3602b6b7b8d
Bozo Dragojevic <bozzo@digiverse.si>
2016-02-03 09:52:23 +0100

    adapt ci setup to the inlining change

--- scripts/develop.sh
+++ scripts/develop.sh
@@ -5,6 +5,4 @@ set -ex
 cwd=$(pwd)
 cd $(dirname "$0")/..
 pip install -r requirements.txt
-mvn install && echo "Don't forget to re-run mvn install if changing the quark java integrations"
-npm link runtime/js-core
-npm link runtime/js-node
+npm install ws
