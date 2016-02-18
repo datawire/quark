f27ef9c6b40a03172a88697eeadeb1227862fbea
Rafael Schloming <rhs@alum.mit.edu>
2016-02-18 12:04:24 -0500

    Changed version to datawire-quarkdev, 0.4.12 (doc 1).

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ except ImportError:
     sys.stderr.write("Could not import bootstrap theme. Is it installed?%s" % os.linesep)
     better_theme = False
 
-__version__ = '0.4.11'
+__version__ = '0.4.12'
 __doc_version__ = '1'
 #__branch__ = 'master'
 #__doc_product_version__ = '0.4'
