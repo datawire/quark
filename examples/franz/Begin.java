0af5d3d507a01a1b3204b32f3ef07f215345f18f
brikelly <bkelly@datawire.io>
2016-03-21 19:10:11 -0400

    Removing franz example until its build script can be fixed

--- examples/franz/Begin.java
+++
@@ -1,9 +0,0 @@
-import franz.Topic;
-
-public class Begin {
-    public static void main(String [] args) {
-        Topic t = new Topic("http://127.0.0.1:8080", "T");
-        t.push("sequence:10:fdsa");
-        t.push("square:fdsa");
-    }
-}
