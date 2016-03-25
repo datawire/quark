d0a1acb575f7d19d976de6ba252e881d9b6e2973
brikelly <bkelly@datawire.io>
2016-03-24 21:05:12 -0400

    Changed version to datawire-quarkdev, 0.4.16 (doc 1).

--- quarkc/_metadata.py
+++ quarkc/_metadata.py
@@ -19,8 +19,8 @@ __all__ = [
     "__license__", "__copyright__",
 ]
 
-__title__ = 'datawire-quark'
-__version__ = '0.4.15'
+__title__ = 'datawire-quarkdev'
+__version__ = '0.4.16'
 
 __summary__ = "Quark: an IDL for high level (micro)service interfaces"
 __uri__ = "http://datawire.github.io/quark/"
