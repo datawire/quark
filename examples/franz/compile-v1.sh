0c8822cb4c4cc863e4784e2b3c42da839e38f110
Philip Lombardi <plombardi@datawire.io>
2016-04-07 11:35:23 -0400

    readme fixes and move pip installs to readme rather than scripts

--- examples/franz/compile-v1.sh
+++ examples/franz/compile-v1.sh
@@ -3,9 +3,6 @@ set -e
 set -u
 set -o pipefail
 
-echo "--> Installing sample python dependencies..."
-pip install -U docopt flask
-
 echo "--> Compiling Franz 1.0 Quark..."
 quark install --python franz-1.0.q
 quark install --javascript franz-1.0.q
