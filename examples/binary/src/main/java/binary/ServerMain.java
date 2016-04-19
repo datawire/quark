a59cf1be1cb5e85e5687b48b8607f7b6b2887d25
Abhay Saxena <ark3@timonium.local>
2016-04-19 17:32:50 -0400

    Add binary server output on start

--- examples/binary/src/main/java/binary/ServerMain.java
+++ examples/binary/src/main/java/binary/ServerMain.java
@@ -7,6 +7,7 @@ public class ServerMain {
 
     public static void main(String[] args) {
         quark.Runtime runtime = Context.runtime();
+        System.out.println("Java binary server starting");
         runtime.serveWS("ws://127.0.0.1:8910/binary", new Binary(runtime));
     }
 
