a485e86403a2a9b0fe6956d2a2ca6db73f3d0d48
Abhay Saxena <ark3@timonium.local>
2016-04-22 14:38:45 -0400

    Add printing nonsense to help debug Travis test failures

--- examples/helloRPC/gen-test/execute.py
+++ examples/helloRPC/gen-test/execute.py
@@ -31,7 +31,16 @@ for server_target, server_cmd, _, server_pattern, server_nocmp in targets:
             key = server_target, client_target
             suffix = " # " + server_target if server_target != client_target else ""
             client_captures[key] = capture(client_cmd + suffix, nocmp=client_nocmp)
-            assert "Responding to [Hello from %s!] from %s" % (client_pattern, server_pattern) in \
-                   client_captures[key].output
+            tmp = server_bg.get_captured().output
+            try:
+                assert "Responding to [Hello from %s!] from %s" % (client_pattern, server_pattern) in \
+                       client_captures[key].output
+            except:
+                print "Client output", client_target, "->", server_target, "==" * 15
+                print client_captures[key].output
+                print "Server output", client_target, "->", server_target, "==" * 15
+                print tmp
+                print "==" * 30
+                raise
     sleep(1.0)
     server_captures[server_target] = server_bg.get_captured()
