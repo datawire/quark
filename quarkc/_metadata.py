82aaeae55d1a36fec95555adfab85cfc96bc1171
Bozo Dragojevic <bozzo@digiverse.si>
2016-04-26 19:00:54 +0200

    next version calculator

--- quarkc/_metadata.py
+++ quarkc/_metadata.py
@@ -20,7 +20,7 @@ __all__ = [
 ]
 
 __title__ = 'datawire-quarkdev'
-__version__ = '0.5.3'
+__version__ = '0.5.2'
 
 __summary__ = "Quark: an IDL for high level (micro)service interfaces"
 __uri__ = "http://datawire.github.io/quark/"
