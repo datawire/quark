dedb94eed819a14cfa068b314bba9b0d50537ffc
Bozo Dragojevic <bozzo@digiverse.si>
2016-05-05 16:24:38 +0200

    old gem gempath does not put user folder at the beginning, hunt it down with grep

--- .travis/sanitize.sh
+++ .travis/sanitize.sh
@@ -1,13 +1,14 @@
 
 function sanitize() {
     PATH=$(echo "$PATH" | tr : \\n |
-                  grep -v -e /\\.local/ -e /\\.rvm/ -e /\\.gem/ |
+                  grep -vF -e /.local/ -e /.rvm/ -e /.gem/ |
                   tr \\n : | sed -e's/:$//' )
     rm -fr ~/.rvm
     rm -fr ~/.local
     unset $(env | grep -Fe ~/.rvm | cut -d= -f1)
     echo "install: --user-install" > ~/.gemrc
     echo "gempath at $1 is $(gem env gempath)"
-    PATH="$PATH":$(gem env gempath | cut -d: -f1)/bin
+    PATH="$PATH":$(gem env gempath | tr : \\n |
+                          grep -F -e "$HOME/.gem/" | head -1)/bin
     .travis/env.sh $1
 }
