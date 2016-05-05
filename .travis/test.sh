8cc83edca00241f3af746094b79e62516430b874
Bozo Dragojevic <bozzo@digiverse.si>
2016-05-05 08:16:22 +0200

    Warmup maven before testing examples

--- .travis/test.sh
+++ .travis/test.sh
@@ -11,6 +11,18 @@ if [[ -f ~/.nvm/nvm.sh ]]; then
     source ~/.nvm/nvm.sh
 fi
 
+case "$TEST_SUITE" in
+    examples)
+        # First example test sometimes times out due to quark install taking too long
+        # Seems like an empty maven cache issue
+        q=/tmp/maven_cache_warmup.q
+        echo 'void main(List<String> args) {}' > $q
+        quark install --java $q
+        quark run --java $q
+        ;;
+    *)
+        ;;
+esac
 
 py.test -v $TEST_SUITE --durations=10
 
