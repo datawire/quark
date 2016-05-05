a9d0ddc5aaa2a144842934943050422ff64d3532
Bozo Dragojevic <bozzo@digiverse.si>
2016-05-05 11:05:20 +0200

    Why is it so hard to get ruby going

--- .travis/test.sh
+++ .travis/test.sh
@@ -32,6 +32,7 @@ java -version 2>&1 | grep -Fe 'java version "1.7.0'
 python -c 'import sys; print(sys.version); sys.exit(int(sys.version_info[:3] < (2,7,6) or (3,0,0) < sys.version_info[:3]))'
 node --version
 ruby --version
+type ruby
 ruby --version 2>&1 | grep -Fe 'ruby 2.3.'
 mvn --version
 mvn --version 2>&1 | grep -Fe 'Apache Maven 3.'
