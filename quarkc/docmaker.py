dab068a619ab1bac5aacba22e5e353e4375a0ac4
Abhay Saxena <ark3@datawire.io>
2016-05-09 14:35:31 -0400

    fix(apidoc): Overhaul generated JSON to be simpler and more complete

--- quarkc/docmaker.py
+++ quarkc/docmaker.py
@@ -16,17 +16,22 @@
 Output JSON to enable Quark-specific documentation generation.
 """
 
+import cgi
 import json
+from collections import OrderedDict
 
 import ast
 from .helpers import doc as get_doc_annotations
 from .coder import Coder
 from .dispatch import dispatch
 
-coder = Coder()
-
 
 class MultiNamespace(ast.Package):
+    """
+    A namespace a.b.c can be defined in more than one location in a file
+    and even over multiple files. This object collects multiple sets of
+    definitions associated with a namespace a.b.c and combines them.
+    """
 
     def append(self, ns):
         assert isinstance(ns, ast.Package), (ns.name, ns.__class__.__name__)
@@ -36,15 +41,7 @@ class MultiNamespace(ast.Package):
 
 class Documenter(object):
     """
-    defn
-     distunit
-     namespace
-     callable
-      function
-      macro
-     class
-      interface
-      primitive
+    Collect namespaces in the order they were first defined.
     """
 
     def __init__(self):
@@ -52,8 +49,7 @@ class Documenter(object):
         self.ns_path = []
 
         # Collected total state
-        self.ns_order = []
-        self.namespaces = {}  # dotted name -> list of definitions
+        self.namespaces = OrderedDict()  # dotted name -> list of definitions
 
     def get_curr_namespace(self):
         return ".".join(str(ns.name) for ns in self.ns_path)
@@ -70,7 +66,6 @@ class Documenter(object):
 
         dotted = self.get_curr_namespace()
         if dotted not in self.namespaces:
-            self.ns_order.append(dotted)
             self.namespaces[dotted] = MultiNamespace(ns.name, [])
         self.namespaces[dotted].append(ns)
 
@@ -78,179 +73,125 @@ class Documenter(object):
 
 
 def get_doc(node):
+    """
+    Return a node's documentation as a string, pulling from annotations
+    or constructing a simple fake as needed.
+    """
     res = " ".join(get_doc_annotations(node))
     if not res:
         res = "(%s)" % node.__class__.__name__.lower()
     return res
 
 
+def get_code(node, coder=Coder()):
+    """
+    Return a node's code
+    """
+    return cgi.escape(str(coder.code(node)), quote=True)
+
+
 def get_type_name(typed):
-    return str(coder.code(typed.type)).replace("<", "&lt;").replace(">", "&gt;")
+    """
+    Return a typed node's type as an HTML-friendly string.
+    """
+    return get_code(typed.type)
 
 
-@dispatch(ast.DistUnit)
-def make_doc_structure(du, doc, path):
-    return {}
+def get_child_path(path, node):
+    if path:
+        return path + "." + str(node.name)
+    else:
+        return str(node.name)
 
 
-@dispatch(ast.Package)  # namespace
-def make_doc_structure(ns, doc, dotted):
-    path = dotted.split(".")
-    res = [dict(name=dotted, description=get_doc(ns), path=path)]
-    for dfn in ns.definitions:
-        if isinstance(dfn, ast.Package):
-            continue
-        json = make_doc_structure(dfn, doc, dotted + "." + str(dfn.name))
-        if json:
-            res.append(json)
+def make_struct(node, path):
+    res = OrderedDict()
+    res["name"] = str(node.name)
+    res["path"] = path
+    res["description"] = get_doc(node)
     return res
 
 
-def not_make_doc_structure(ns, doc, dotted):
-    path = dotted.split(".")
-    res = dict(name=dotted,
-               kind="function",      # namespace
-               description="Namespace " + str(ns.name),
-               tags=[],
-               members=dict(instance=[], static=[]),
-               memberof=".".join(path[:-1]),
-               scope="static",
-               path=path)
-    for dfn in ns.definitions:
-        if isinstance(dfn, ast.Package):
-            continue
-        json = make_doc_structure(dfn, doc, dotted + "." + str(dfn.name))
-        if json:
-            res["members"]["static"].append(json)
-    return res
+@dispatch(ast.AST)
+def make_doc_structure(node, path):
+    raise ValueError("Unhandled node type %r at %s" % (node.__class__.__name__, get_child_path(path, node)))
 
 
-def function_helper(node, doc, dotted):
-    path = dotted.split(".")
-    res = dict(name=dotted,
-               kind="function",
-               tags=[],
-               description=get_doc(node),
-               context=dict(loc=dict(start=dict(line=node.line, column=node.column),
-                                     end=dict(line=node.line, column=node.column)),  # XXX
-                            file=node.filename, code=""),
-               members=dict(instance=[], static=[]),
-               memberof=".".join(path[:-1]),
-               scope="static",
-               params=[],
-               returns=[],
-               path=path)
-    for param in node.params:
-        res["params"].append(dict(title="param", description=get_doc(param), name=str(param.name),
-                                  type=dict(type="NameExpression", name=get_type_name(param))))
-    if node.type and get_type_name(node) != "void":
-        res["returns"].append(dict(title="returns", description="(return value)",
-                                   type=dict(type="NameExpression", name=get_type_name(node))))
-    return res
-
-
-@dispatch(ast.Function)
-def make_doc_structure(node, doc, dotted):
-    res = function_helper(node, doc, dotted)
-    #res["description"] = "Function " + str(node.name)
-    return res
-
-
-@dispatch(ast.Macro)
-def make_doc_structure(node, doc, dotted):
-    res = function_helper(node, doc, dotted)
-    #res["description"] = "Macro " + str(node.name)
-    return res
-
-
-@dispatch(ast.Method)
-def make_doc_structure(node, doc, dotted):
-    res = function_helper(node, doc, dotted)
-    #res["description"] = "Method " + str(node.name)
-    res["scope"] = "static" if node.static else "instance"
-    return res
-
-
-@dispatch(ast.MethodMacro)
-def make_doc_structure(node, doc, dotted):
-    res = function_helper(node, doc, dotted)
-    #res["description"] = "MethodMacro " + str(node.name)
-    res["scope"] = "static" if node.static else "instance"
-    return res
-
-
-@dispatch(ast.Constructor)
-def make_doc_structure(node, doc, dotted):
-    res = function_helper(node, doc, dotted)
-    #res["description"] = "Constructor " + str(node.name)
-    return res
-
-
-@dispatch(ast.ConstructorMacro)
-def make_doc_structure(node, doc, dotted):
-    res = function_helper(node, doc, dotted)
-    #res["description"] = "ConstructorMacro " + str(node.name)
-    return res
-
-
-@dispatch(ast.Field)
-def make_doc_structure(node, doc, dotted):
-    res = dict(name=str(node.name), description=get_doc(node),
-               type=dict(type="NameExpression", name=get_type_name(node)))
-    return res
-
-
-def class_helper(node, doc, dotted):
-    path = dotted.split(".")
-    res = dict(name=dotted,
-               kind="class",
-               tags=[],
-               description=get_doc(node),
-               context=dict(loc=dict(start=dict(line=node.line, column=node.column),
-                                     end=dict(line=node.line, column=node.column)),  # XXX
-                            file=node.filename, code=""),
-               members=dict(instance=[], static=[]),
-               properties=[],
-               memberof=".".join(path[:-1]),
-               scope="static",
-               path=path)
+@dispatch(ast.Package)  # aka namespace
+def make_doc_structure(node, path):
+    res = make_struct(node, path)
+    res["kind"] = "namespace"
+    res["definitions"] = []
+    child_path = get_child_path(path, node)
     for dfn in node.definitions:
-        # field / constructor / method / constructor_macro / method_macro
-        if str(dfn.name) in "_getClass _getField _setField".split():
-            continue
-        json = make_doc_structure(dfn, doc, dotted + "." + str(dfn.name))
-        if isinstance(dfn, ast.Field):
-            res["properties"].append(json)
-        else:
-            res["members"][json["scope"]].append(json)
+        st = make_doc_structure(dfn, child_path)
+        res["definitions"].append(st)
     return res
 
 
 @dispatch(ast.Class)
-def make_doc_structure(node, doc, path):
-    res = class_helper(node, doc, path)
-    #res["description"] = "Class " + str(node.name)
+def make_doc_structure(node, path):
+    res = make_struct(node, path)
+    kind = node.__class__.__name__.lower()
+    assert kind in "class interface primitive".split(), (path, node.name, kind)
+    res["kind"] = kind
+    res["bases"] = [get_code(base) for base in node.bases]
+    res["parameters"] = [get_code(parameter) for parameter in node.parameters]
+    child_path = get_child_path(path, node)
+    res["definitions"] = []
+    for dfn in node.definitions:
+        # field / constructor / method / constructor_macro / method_macro
+        if str(dfn.name) in "_getClass _getField _setField".split():
+            continue
+        st = make_doc_structure(dfn, child_path)
+        res["definitions"].append(st)
     return res
 
 
-@dispatch(ast.Interface)
-def make_doc_structure(node, doc, path):
-    res = class_helper(node, doc, path)
-    #res["description"] = "Interface " + str(node.name)
+@dispatch(ast.Field)
+def make_doc_structure(node, path):
+    res = make_struct(node, path)
+    res["kind"] = "static field" if node.static else "instance field"
+    res["type"] = get_type_name(node)
     return res
 
 
-@dispatch(ast.Primitive)
-def make_doc_structure(node, doc, path):
-    res = class_helper(node, doc, path)
-    #res["description"] = "Primitive " + str(node.name)
+@dispatch(ast.Callable)
+def make_doc_structure(node, path):
+    # function, macro, constructor, instance method, static method, constructor macro, method macro
+    res = make_struct(node, path)
+    kind = node.__class__.__name__.lower()
+    if kind == "method":
+        kind = "static method" if node.static else "instance method"
+    elif kind == "constructormacro":
+        kind = "constructor macro"
+    elif kind == "methodmacro":
+        kind = "method macro"
+    res["kind"] = kind
+    res["type"] = get_type_name(node)
+    child_path = get_child_path(path, node)
+    res["parameters"] = []
+    for parameter in node.params:
+        st = make_doc_structure(parameter, child_path)
+        res["parameters"].append(st)
+    return res
+
+
+@dispatch(ast.Param)
+def make_doc_structure(node, path):
+    res = make_struct(node, path)
+    res["kind"] = "parameter"
+    res["type"] = get_type_name(node)
     return res
 
 
 def make_docs_json(root, filename):
     doc = Documenter()
     root.traverse(doc)
-    res = sum([make_doc_structure(doc.namespaces[dotted], doc, dotted)
-               for dotted in doc.ns_order], [])
+    res = []
+    for dotted, ns in doc.namespaces.items():
+        path = ".".join(dotted.split(".")[:-1])
+        structure = make_doc_structure(ns, path)
+        res.append(structure)
     with open(filename, "wb") as out:
         json.dump(res, out, indent=4)
