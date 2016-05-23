94f162bb9647b0da1d3173bdb19380d3bf9874c4
Bozo Dragojevic <bozzo@digiverse.si>
2016-05-23 09:41:38 +0200

    Update expected results

--- quarkc/test/ffi/expected/py/org_example_foo/setup.py
+++ quarkc/test/ffi/expected/py/org_example_foo/setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 
 setup(name="org_example_foo",
       version="0.1.0",
-      install_requires=["quark==0.0.1"],
+      install_requires=["wheel", "quark==0.0.1"],
+      setup_requires=["wheel"],
       py_modules=[],
       packages=['org', 'org.example', 'org.example.foo', 'org_example_foo_md'])
--- quarkc/test/ffi/expected/py/overlapping_namespaces/setup.py
+++ quarkc/test/ffi/expected/py/overlapping_namespaces/setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 
 setup(name="overlapping_namespace",
       version="0.1.0",
-      install_requires=["quark==0.0.1", "org_example_foo==0.1.0"],
+      install_requires=["wheel", "quark==0.0.1", "org_example_foo==0.1.0"],
+      setup_requires=["wheel"],
       py_modules=[],
       packages=['org', 'org.example', 'org.example.bar', 'overlapping_namespace_md'])
