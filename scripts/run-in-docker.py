eaf821886f90e184ef72f932a3a31a8c12117a54
Itamar Turner-Trauring <itamar@datawire.io>
2016-05-25 14:49:49 -0400

    First pass of semi-working quark-in-Docker transparently.

---
+++ scripts/run-in-docker.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""
+Run the given arguments within a Docker container fully configured with Quark's requirements.
+
+In order to be as transparent as possible:
+* The root of the git checkout will be bind-mounted in as Docker volume.
+* The root of the git checkout will be working directory.
+* The given commands will be run as the current user's uid and gid.
+"""
+
+import os
+import sys
+import subprocess
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    # Figure out UID of files in Docker; on Linux this will be
+    # currently running user, on Mac it will be the UID of user in VM
+    # which can be different.
+    uid = subprocess.check_output([
+            "docker", "run", "--rm", "-v", "{}:/path".format(ROOT_DIR),
+            "busybox", "stat", "-c", "%u", "/path"]).strip()
+    subprocess.check_call(["docker", "build",
+                           # Tag for resulting image:
+                           "-t", "quark-run-transparently",
+                           # Pass in appropriate UID:
+                           "--build-arg", "userid={}".format(uid),
+                           # Dockerfile to use:
+                           "-f",  "images/run-transparently", "."],
+                          cwd=ROOT_DIR)
+    subprocess.call(["docker", "run",
+                     # Interactive:
+                     "-i", "-t",
+                     # Delete when done:
+                     "--rm",
+                     # Map root directory in as volume:
+                     "-v", "{}:{}".format(ROOT_DIR, ROOT_DIR),
+                     # Run in root directory:
+                     "-w", ROOT_DIR,
+                     # Image to use:
+                     "quark-run-transparently",
+                     ] + sys.argv[1:], cwd=ROOT_DIR)
+
+
+if __name__ == '__main__':
+    main()
