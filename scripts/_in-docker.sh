5aea7d8bfef4d47e70297b4096489844648487d5
Itamar Turner-Trauring <itamar@datawire.io>
2016-05-25 16:46:33 -0400

    It's now actually possible to run Ruby tests with the new Docker setup.

--- scripts/_in-docker.sh
+++ scripts/_in-docker.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Virtualenv for Python:
 source /tmp/run/bin/activate
+
+# Add GEM_HOME to GEM_PATH for Ruby:
+export GEM_PATH=$GEM_PATH:$GEM_HOME
+
+# Configure dependencies:
+python setup.py develop
+
+# Run command passed in by user:
 env "$@"
