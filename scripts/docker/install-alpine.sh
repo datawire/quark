3f85a1973a02a0ffb969ba01b3fe6c0e09bc9bac
Itamar Turner-Trauring <itamar@datawire.io>
2016-06-14 13:40:25 -0400

    Docker bug fixes for scripts/with-docker.py.
    1. Fix user adding in Alpine Linux
    2. Install necessary git package
    3. Give Docker container its own isolated node_module for NPM, instead
       of sharing across runs.

--- scripts/docker/install-alpine.sh
+++ scripts/docker/install-alpine.sh
@@ -2,7 +2,7 @@
 # Install dependencies for Quark inside Alpine:
 apk add --update bash
 apk add --no-cache make gcc libc-dev g++
-apk add --no-cache openjdk8
+apk add --no-cache git openjdk8
 apk add --no-cache python python-dev py-pip py-virtualenv
 apk add --no-cache ruby ruby-dev ruby-bundler ruby-rdoc ruby-irb
 apk add --no-cache nodejs nodejs-dev
