62335f148ce3c08b76c25835ec36716583d4b74e
Itamar Turner-Trauring <itamar@datawire.io>
2016-09-12 10:22:11 -0400

    Changed version to datawire-quark, 1.0.443 (doc 1). [ci skip]

--- quarkc/_metadata.py
+++ quarkc/_metadata.py
@@ -19,8 +19,8 @@ __all__ = [
     "__license__", "__copyright__",
 ]
 
-__title__ = 'datawire-quarkdev'
-__version__ = '1.0.406'
+__title__ = 'datawire-quark'
+__version__ = '1.0.443'
 
 __summary__ = "Quark: an IDL for high level (micro)service interfaces"
 __uri__ = "http://datawire.github.io/quark/"
--- quarkc/lib/quark.q
+++ quarkc/lib/quark.q
@@ -27,7 +27,7 @@ use java junit junit 4.12;
 include io/datawire/quark/runtime/AbstractDatawireRuntime.java;
 include io/datawire/quark/runtime/Builtins.java;
 
-package quark 1.0.406;
+package quark 1.0.443;
 
 import quark.error;
 
