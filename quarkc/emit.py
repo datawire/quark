cb3f5afc8a225270f15f70a1f7e79a58fc63d62b
Bozo Dragojevic <bozzo@digiverse.si>
2016-09-30 22:43:38 +0200

    Add cmdline emit-ir thing

--- quarkc/emit.py
+++ quarkc/emit.py
@@ -1,3 +1,35 @@
+# Copyright 2015 datawire. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Quark IR emitter.
+
+Usage:
+  quark-ir emit [options] --go <files>...
+
+Commands:
+
+  emit    Emit target code for the specified quark IR files in the output directory
+
+Options:
+  -h --help             Show this screen.
+  -v --verbose          Show more detail.
+
+  -o DIR, --output DIR  Target directory for output files. [default: .]
+
+"""
+
 from contextlib import contextmanager
 from .match import *
 from .ir import *
@@ -977,3 +1009,36 @@ def code(assrt, target):
     return "assert_equal(({expected}), ({actual}))".format(
                  expected = code(assrt.expected, target),
                  actual = code(assrt.actual, target))
+
+
+import py.path
+
+def writeout(files, output, verbose):
+    o = py.path.local(output)
+    for name, content in files.items():
+        f = o.join(name)
+        f.write(content, ensure=True)
+        if verbose:
+            print "wrote %s" % f
+
+from docopt import docopt
+
+def main(args):
+    backends = {"--go" : Go,
+                "--java" : Java,
+                "--ruby" : Ruby,
+                "--python" : Python,
+            }
+    for fn in args["<files>"]:
+        with open(fn) as f:
+            ir = IR.load(f.read(), source=fn)
+        for opt, backend in backends.items():
+            if not (opt in args or "--all" in args):
+                continue
+            b = backend()
+            emit(ir, b)
+            writeout(b.files, output=args["--output"], verbose=args["--verbose"])
+ 
+
+def call_main():
+    exit(main(docopt(__doc__)))
