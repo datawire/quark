f53fdaf3089ffeb6372f9942157270cc5aa33a44
Rafael Schloming <rhs@alum.mit.edu>
2016-10-10 21:49:16 -0400

    a real command line for c2

---
+++ quarkc/c2/command.py
@@ -0,0 +1,73 @@
+
+"""
+Quark compiler.
+
+Usage:
+  quark [ -o DIR ] [ (--java | --python | --python3 | --javascript | --ruby | --go)... | --all ] [<file>]...
+  quark -h | --help
+  quark --version
+
+Options:
+  -h --help             Show this screen.
+  --version             Show version.
+  -v --verbose          Show more detail.
+
+  -o DIR, --output DIR  Target directory for output files. [default: output]
+
+  --all                 Install/emit code for all available target languages.
+                        [this is the default if no targets are specified]
+
+  --java                Emit Java code.
+  --ruby                Emit Ruby code.
+  --python              Emit Python 2 code (if you're just emitting --python3 will work too.)
+  --python3             Emit Python 3 code (if you're just emitting --python will work too.)
+  --javascript          Emit JavaScript code.
+  --go                  Emit go code.
+"""
+
+import os
+from docopt import docopt
+from .compiler import Compiler
+from .emit import emit, Java, Python, Ruby, Go
+
+def main(args):
+    java = args["--java"]
+    ruby = args["--ruby"]
+    python = args["--python"]
+    python3 = args["--python3"]
+    javascript = args["--javascript"]
+    go = args["--go"]
+
+    all = args["--all"] or not (java or python or javascript or ruby or python3 or go)
+
+    output = args["--output"]
+
+    targets = []
+    if java or all: targets.append(Java())
+    if python or all: targets.append(Python())
+#    if python3 or all: targets.append(Python3())
+#    if javascript or all: targets.append(Javascript())
+    if ruby or all: targets.append(Ruby())
+    if go or all: targets.append(Go())
+
+    c = Compiler()
+    for fname in args["<file>"]:
+        with open(fname) as f:
+            c.parse(fname, f.read())
+    c.check()
+    pkg = c.compile()
+    for tgt in targets:
+        emit(pkg, tgt)
+        for file in tgt.files.values():
+            fname = os.path.join(output, file.name)
+            dir = os.path.dirname(fname)
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+            with open(fname, "write") as f:
+                f.write("%s\n" % str(file))
+
+def call_main():
+    exit(main(docopt(__doc__, version="Quark %s" % 2.0)))
+
+if __name__ == "__main__":
+    call_main()
---
+++ quarkc/test/c2/program.q
@@ -0,0 +1,99 @@
+namespace quark {
+    primitive int {
+        int __add__(int other);
+        int __sub__(int other);
+        int __mul__(int other);
+        bool __lt__(int other);
+        bool __eq__(int other);
+        String toString();
+    }
+
+    primitive bool {
+        bool __eq__(bool other);
+        String toString();
+    }
+
+    primitive String {
+        bool __lt__(String other);
+        bool __eq__(String other);
+        String toString();
+    }
+
+    primitive void {}
+
+    primitive Map<K,V> {
+        V __get__(K key);
+        void __set__(K key, V value);
+    }
+}
+
+namespace math {
+    int fib(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return fib(n-1) + fib(n-2);
+        }
+    }
+}
+
+import math;
+
+namespace other {
+    int fib2(int n) {
+        return 2*fib(n);
+    }
+
+    class Foo {
+
+        String asdf;
+
+        int foo() {
+            asdf = "fdsa";
+            return fib2(3);
+        }
+
+        String bar() {
+            int count = 0;
+            while (count < 3) {
+                count = count + 1;
+            }
+            Box<String> box = new Box<String>();
+            Box<int> ibox = new Box<int>();
+            ibox.set(3);
+            box.set("three");
+            Foo f = new Foo();
+            Map<int,String> m = new Map<int,String>();
+            MultiBox<int> mbi = new MultiBox<int>();
+            return foo().toString();
+        }
+    }
+
+    class Box<T> {
+        T contents;
+
+        T get() {
+            return contents;
+        }
+
+        void set(T value) {
+            contents = value;
+        }
+    }
+
+    class MultiBox<T> {
+        Map<String,T> contents;
+
+        MultiBox() {
+            contents = new Map<String,T>();
+        }
+
+        T get(String key) {
+            return contents[key];
+        }
+
+        void set(String key, T value) {
+            contents[key] = value;
+        }
+    }
+}
