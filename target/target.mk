d1234b88f44f4e07137a8b84a820890657d03c7d
Bozo Dragojevic <bozzo@digiverse.si>
2016-10-17 14:28:47 +0200

    Factor out common target makefile stuff. Fix error propagation
    toplevel makefile was eating exitcodes and the `inside` macro, too

---
+++ target/target.mk
@@ -0,0 +1,19 @@
+.PHONY: test clean default
+
+default:
+	echo make emit   - generate target code
+	echo make clean  - remove generated code
+	echo make test   - run the tests
+
+track = echo "$(1) directory \`$$PWD'"
+
+inside = cd $(1) && { $(call track,Entering);  $(2); rc=$$?; $(call track,Leaving); exit $$rc; }
+
+clean:
+	rm -fr "$(CURDIR)/emit"
+
+EMIT_DIR = emit
+
+emit:
+	mkdir -p $(EMIT_DIR)
+	quark-ir emit -o $(EMIT_DIR) $(EMIT_TARGET) ../../quarkc/test/c2/ffi_ir/*.ir
