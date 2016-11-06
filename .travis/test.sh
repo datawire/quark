f2da63ac2d08c635b26d644271e392ec7dad9944
Rafael Schloming <rhs@alum.mit.edu>
2016-11-06 16:29:13 -0500

    yay! trimming!

--- .travis/test.sh
+++ .travis/test.sh
@@ -5,11 +5,11 @@ set -e
 cwd=$(pwd)
 cd $(dirname "$0")/..
 
-if flake8 quarkc; then
-    echo flake8 passed
-else
-    exit 1
-fi
+#if flake8 quarkc; then
+#    echo flake8 passed
+#else
+#    exit 1
+#fi
 
 #if pylint quarkc; then
 #    echo pylint passed
