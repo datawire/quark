ecd1e7dcfbd2e9487fcbadcc3858e477257014ec
Bozo Dragojevic <bozzo@digiverse.si>
2016-11-18 14:41:06 +0100

    Add a content-indexed IR cache (TODO: content-index the compiler, too)

--- quarkc/command.py
+++ quarkc/command.py
@@ -34,6 +34,7 @@ from .compiler import Compiler
 from .exceptions import QuarkError
 from .emit import emit, Java, Python, Ruby, Go, Javascript, transform, reconstruct
 from .helpers import is_newer
+from .ir import IR
 
 import stats
 
@@ -46,11 +47,15 @@ def write_ir(fname, pkg):
         ensure_dir(fname)
         with open(fname, "write") as f:
             f.write("%s\n" % repr(pkg))
+        if VERBOSE:
+            print "wrote %s" % fname
 
 def main(args):
+    global VERBOSE
     verbose = 0
     if args["--verbose"]:
         verbose = 1
+    VERBOSE = verbose
 
     java = args["--java"]
     ruby = args["--ruby"]
@@ -66,45 +71,72 @@ def main(args):
 
     targets = []
     if java or all: targets.append(Java())
-    if python or all: targets.append(Python())
-#    if python3 or all: targets.append(Python3())
+    if python or python3 or all: targets.append(Python())
     if javascript or all: targets.append(Javascript())
     if ruby or all: targets.append(Ruby())
     if go or all: targets.append(Go())
 
     with stats.charge("index"):
         index = {}
+        pkgsum = hashlib.sha1()
         for fname in args["<file>"]:
                 with open(fname) as f:
                     content = f.read()
                     index[fname] = dict(
                         content=content,
                         sha = hashlib.sha1(content).hexdigest())
+                    pkgsum.update(index[fname]["sha"])
+        pkgsum = pkgsum.hexdigest()
+        ir_base = os.path.join("/tmp", os.path.basename(args["<file>"][0]) + "-" + pkgsum)
+        ir_name = ir_base + ".ir"
 
-    c = Compiler(verbose=verbose)
-    for fname in args["<file>"]:
-        cname = "%sp" % fname
-        if is_newer(cname, fname) and not args["--force"]:
-            with stats.charge("unpickle"), open(cname) as f:
-                unp = pickle.Unpickler(f)
-                data = unp.load()
-                if isinstance(data, tuple) and len(data) == 2 and data[0] == index[fname]["sha"]:
-                    if verbose:
-                        print "Found valid parse cache %s" % cname
-                    c.load(data[1])
-                    continue
-        ast = c.parse(fname, index[fname]["content"])
-        if ast:
-            with stats.charge("pickle"), open(cname, "write") as fp:
-                pickler = pickle.Pickler(fp, -1)
-                pickler.dump((index[fname]["sha"], ast))
+    pkg = None
 
-    try:
-        c.check()
-    except QuarkError, e:
-        return e
+    ir_fresh = is_newer(ir_name, *args["<file>"])
+    if bool(ir_fresh) and not args["--force"]:
+        with stats.charge("load-ir-cache"):
+            try:
+                print "Loading IR cache %s" % ir_name
+                pkg = IR.load_path(ir_name)
+                if verbose:
+                    print "Loaded valid %s" % ir_name
+            except:
+                print "Loading IR failed"
+                raise
+    else:
+        print "%s the IR cache %s %s %s" % (
+            (args["--force"] and "Ignored" or "Cannot find"),
+            bool(ir_fresh), ir_fresh.explanation, ir_fresh.target)
 
-    pkg = c.compile()
+    if pkg is None:
+        c = Compiler(verbose=verbose)
+        for fname in args["<file>"]:
+            cname = "%sp" % fname
+            if is_newer(cname, fname) and not args["--force"]:
+                with stats.charge("unpickle"), open(cname) as f:
+                    unp = pickle.Unpickler(f)
+                    data = unp.load()
+                    if isinstance(data, tuple) and len(data) == 2 and data[0] == index[fname]["sha"]:
+                        if verbose:
+                            print "Found valid parse cache %s" % cname
+                        c.load(data[1])
+                        continue
+            ast = c.parse(fname, index[fname]["content"])
+            if ast:
+                with stats.charge("pickle"), open(cname, "write") as fp:
+                    pickler = pickle.Pickler(fp, -1)
+                    pickler.dump((index[fname]["sha"], ast))
+
+        if verbose:
+            print "Compiling...."
+        try:
+            c.check()
+        except QuarkError, e:
+            return e
+
+        pkg = c.compile()
+        write_ir(ir_name, pkg)
+
     for tgt in targets:
         with stats.charge("emit"):
             files = emit(pkg, tgt)
@@ -117,15 +149,13 @@ def main(args):
                 f.write(content)
 
     if ir:
-        fname = os.path.join(output, os.path.basename(args["<file>"][0]))
-        write_ir(fname + ".ir", pkg)
-        fname += "-reconstruct"
+        ir_base += "-reconstruct"
         pkg = reconstruct(pkg)
-        write_ir(fname + ".ir", pkg)
-        fname += "-transform"
+        write_ir(ir_base + ".ir", pkg)
+        ir_base += "-transform"
         for tgt in targets:
             ext = "-%s.ir" % tgt.__class__.__name__
-            write_ir(fname + ext, transform(pkg, tgt))
+            write_ir(ir_base + ext, transform(pkg, tgt))
 
 
 def call_main():
