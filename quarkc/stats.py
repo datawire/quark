0dab28f278b2c35d709693f1cc384c28dfdea496
Bozo Dragojevic <bozzo@digiverse.si>
2016-11-18 11:51:45 +0100

    Pretty print timing stats. histograms! yay!

--- quarkc/stats.py
+++ quarkc/stats.py
@@ -36,7 +36,22 @@ def charge(tag):
         STATS[tag] = Stats()
     return STATS[tag]
 
-def dump(out):
+def dump(out, width=100):
+    total = sum(stat.elapsed for stat in STATS.values())
+    key_width = max(map(len, STATS))
+    elapsed_width = max(len("%5.3f" % stat.elapsed) for stat in STATS.values())
+    count_width = max(len(str(stat.count)) for stat in STATS.values())
+    hist_width = width - (key_width + 3 + elapsed_width + 3 + count_width + 2)
+    if hist_width < 10:
+        hist_width = 0
     for k in sorted(STATS):
         stat = STATS[k]
-        out.write("%s=%s/%s\n" % (k, stat.elapsed, stat.count))
+        if hist_width:
+            hist = " |" + "#" * int(hist_width * stat.elapsed / total)
+        else:
+            hist = ""
+        out.write("{0:{key_width}} = {1:{elapsed_width}.3f} / {2:{count_width}}{3}\n".format(
+            k, stat.elapsed, stat.count, hist,
+            key_width = key_width,
+            elapsed_width = elapsed_width,
+            count_width = count_width))
