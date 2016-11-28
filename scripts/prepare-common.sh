23a13990831e3bd5b1979e4bb29723760ad41d6d
Bozo Dragojevic <bozzo@digiverse.si>
2016-11-28 19:04:31 +0100

    don't die of missing stamps

--- scripts/prepare-common.sh
+++ scripts/prepare-common.sh
@@ -8,7 +8,7 @@ cd $(dirname "$0")/..
 csum=$(cat requirements.txt setup.py install-requirements.txt | shasum | cut -d\  -f 1)
 CACHE_DIR=~/.quark/wheel-cache
 CACHE_STAMP=$CACHE_DIR/.quark.stamp
-stamp=$(test -f $CACHE_STAMP && cat $CACHE_STAMP)
+stamp=$(test -f $CACHE_STAMP && cat $CACHE_STAMP || echo xxx)
 if [ "$stamp" != "$csum" ]; then
     rm -fr $CACHE_DIR
     pip wheel --wheel-dir $CACHE_DIR -r requirements.txt
