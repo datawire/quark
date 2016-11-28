59a92bc30907d586fd7c9f0012998bd85b364d9e
Bozo Dragojevic <bozzo@digiverse.si>
2016-11-28 18:59:41 +0100

    Try caching python wheels

--- scripts/prepare-common.sh
+++ scripts/prepare-common.sh
@@ -4,4 +4,14 @@ set -ex
 
 cwd=$(pwd)
 cd $(dirname "$0")/..
-pip install -r requirements.txt
+
+csum=$(cat requirements.txt setup.py install-requirements.txt | shasum | cut -d\  -f 1)
+CACHE_DIR=~/.quark/wheel-cache
+CACHE_STAMP=$CACHE_DIR/.quark.stamp
+stamp=$(test -f $CACHE_STAMP && cat $CACHE_STAMP)
+if [ "$stamp" != "$csum" ]; then
+    rm -fr $CACHE_DIR
+    pip wheel --wheel-dir $CACHE_DIR -r requirements.txt
+    echo $csum > $CACHE_STAMP
+fi
+pip install --no-index --find-links=$CACHE_DIR -r requirements.txt
